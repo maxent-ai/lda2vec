f2cb714683d59b93a022c8f7c68ca870997a671e
bharath g s <royalkingpin@gmail.com>
2019-02-07 12:03:02 +0530

    init files

---
+++ README.md
@@ -0,0 +1,23 @@
+# lda2vec
+
+-----
+
+**Table of Contents**
+
+* [Installation](#installation)
+* [License](#license)
+
+## Installation
+
+lda2vec is distributed on [PyPI](https://pypi.org) as a universal
+wheel and is available on Linux/macOS and Windows and supports
+Python 3.6+.
+
+```bash
+$ pip install lda2vec
+```
+
+## License
+
+lda2vec is distributed under the terms of the
+[MIT License](https://choosealicense.com/licenses/mit).
---
+++ setup.py
@@ -0,0 +1,48 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('lda2vec/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = []
+
+setup(
+    name='lda2vec',
+    version=version,
+    description='',
+    long_description=readme,
+    author='Bharath G.S',
+    author_email='royalkingpin@gmail.com',
+    maintainer='Bharath G.S',
+    maintainer_email='royalkingpin@gmail.com',
+    url='https://github.com/_/lda2vec',
+    license='MIT',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
